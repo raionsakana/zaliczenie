ec6cea4e59b055c8dc98ff1b79dc35045aaf3f6e
raionsakana <52761148+raionsakana@users.noreply.github.com>
2020-06-08 18:27:14 +0200

    testIfMilkProviderMethodHeatInvoked

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
+import edu.iis.mto.testreactor.coffee.milkprovider.MilkProviderException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -83,6 +84,32 @@ public class CoffeeMachineTest {
         this.coffeeMachine.make(coffeOrder);
 
         verify(this.coffeeReceipes, times(3)).getReceipe(CoffeType.LATTE);
+    }
+
+    @Test
+    public void testIfMilkProviderMethodHeatInvoked() throws MilkProviderException {
+        Map<CoffeeSize, Integer> waterMap = new HashMap<>();
+        waterMap.put(CoffeeSize.STANDARD, 10);
+
+        CoffeeReceipe coffeeReceipe = CoffeeReceipe
+                .builder()
+                .withMilkAmount(3)
+                .withWaterAmounts(waterMap)
+                .build();
+
+        Optional<CoffeeReceipe> coffeeReceipeOptional = Optional.of(coffeeReceipe);
+        CoffeOrder coffeOrder = mock(CoffeOrder.class);
+
+        when(coffeOrder.getSize()).thenReturn(CoffeeSize.STANDARD);
+        when(coffeOrder.getType()).thenReturn(CoffeType.LATTE);
+
+        when(this.grinder.canGrindFor(CoffeeSize.STANDARD)).thenReturn(true);
+        when(this.grinder.grind(CoffeeSize.STANDARD)).thenReturn(2.3);
+        when(this.coffeeReceipes.getReceipe(CoffeType.LATTE)).thenReturn(coffeeReceipeOptional);
+
+        this.coffeeMachine.make(coffeOrder);
+
+        verify(this.milkProvider, times(1)).heat();
     }
 
 }
